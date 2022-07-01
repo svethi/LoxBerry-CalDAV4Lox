0f147d9ed3d320fd5cadc37f7723733ae4853b01
svethi <svethi@loxberry.de>
2022-07-01 23:19:46 +0200

    errorhandling curl

--- webfrontend/html/caldav.php
+++ webfrontend/html/caldav.php
@@ -112,7 +112,7 @@ function curl_get_contents($url,$user,$pass) {
 	   curl_setopt($ch, CURLOPT_HTTPHEADER, array('User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'));
 	   $result = curl_exec($ch);
 	   if ($result === false) {
-		$result = curl_error($ch);
+		$result = curl_strerror(curl_errno($ch));
 	   }
 	   return $result;
 }
